a9e24f78b15ffbf38ea81d3881e84b344f7fb9b3
trv48226 <58450961+trv48226@users.noreply.github.com>
2020-05-13 21:35:16 -0500

    Gui interacting with Model (Cat win not working)

--- chatNoir/src/chatNoir/Model.java
+++ chatNoir/src/chatNoir/Model.java
@@ -8,85 +8,70 @@ public class Model {
 
 	private Boolean ownerTurn;
 	private Colors[][] board;
-	private Boolean isVisited;
-	private Colors getLastCatMove;
-	
-	public String getBoardColor(int i, int j) {
-		if(board[i][j].getColor() == "white") {
-			return ("-fx-background-color: white");
-		}else if(board[i][j].getColor() == "Grey") {
-			return("-fx-background-color: #A9A9A9");
-		}else if(board[i][j].getColor() == "edge"){
-			return("-fx-background-color: #DCDCDC");
-		}else {
-			return("-fx-background-color: #FF0000");
-		}
-	}
+	private Boolean[][] isVisited;
+	private Colors lastCatMove;
 	
 	/** A helper object to handle observer pattern behavior */
 	protected PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
 	public Model() {
-		board = new Colors[11][11];
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				Colors temp = new Colors("white", i, j);
-				board[i][j] = temp;
-			}
-		}
-		board[5][5] = new Colors("Red", 5, 5);
-		getLastCatMove = board[5][5];
-		setEdge();
-		setBlockedSquares();
-		ownerTurn = false;
+		startNewGame();
 	}
 
 	public void setBlockedSquares() {
-		System.out.println();
 		for (int i = 0; i < 11; i++) {
 			Random r = new Random();
-			int xValue = r.nextInt(10 - 0) + 0;
-			int yValue = r.nextInt(10 - 0) + 0;
+			int xValue = r.nextInt(11);
+			int yValue = r.nextInt(11);
 			if (!board[xValue][yValue].getColor().equals("Red") && !board[xValue][yValue].equals("Grey")) {
 				board[xValue][yValue].setColor("Grey");
-			//}else {
-			//	i--;
+			}else {
+				i--;
 			}
 		}
 	}
 
 	public void setEdge() {
 		for (int i = 0; i < board.length; i++) {
-			board[0][i].setColor("edge");
+			board[0][i].setColor("Edge");
 		}
 		for (int i = 0; i < board.length; i++) {
-			board[10][i].setColor("edge");
+			board[10][i].setColor("Edge");
 		}
 		for (int i = 0; i < board.length; i++) {
-			board[i][0].setColor("edge");
+			board[i][0].setColor("Edge");
 		}
 		for (int i = 0; i < board.length; i++) {
-			board[i][10].setColor("edge");
+			board[i][10].setColor("Edge");
 		}
 	}
 
-	public Colors getlastCatMove() {
-		return null;
+	public Colors getLastCatMove() {
+		return lastCatMove;
 	}
 
 	public void move(int x, int y) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				isVisited[i][j] = false;
+			}
+		}	
 		if (ownerTurn == false) {
 			if (isValidCatMove(x, y) == true) {
-				board[x][y].setColor("Red");
-				board[getLastCatMove.getxValue()][getLastCatMove.getyValue()].setColor("White");
-				getLastCatMove.setxValue(x);
-				getLastCatMove.setyValue(y);
 				ownerTurn = true;
+				if(board[x][y].getColor().equals("Edge")) {
+					board[lastCatMove.getxValue()][lastCatMove.getyValue()].setColor("Edge");
+				}
+				board[x][y].setColor("Red");
+				board[lastCatMove.getxValue()][lastCatMove.getyValue()].setColor("White");
+				lastCatMove.setxValue(x);
+				lastCatMove.setyValue(y);
 				pcs.firePropertyChange("valid", null, null);
 			} else
 				pcs.firePropertyChange("invalid", null, null);
 		} else {
 			if (isValidOwnerMove(x, y) == true) {
+				ownerTurn = false;
 				board[x][y].setColor("Grey");
 				pcs.firePropertyChange("valid", null, null);
 			} else
@@ -94,21 +79,79 @@ public class Model {
 		}
 	}
 
-	public boolean isValidOwnerMove(int x, int y) {
+	private boolean isValidOwnerMove(int x, int y) {
 		if (board[x][y].getColor().equals("Red") || board[x][y].getColor().equals("Grey"))
 			return false;
 		return true;
 	}
 
-	public boolean isValidCatMove(int x, int y) {
-		int catX = getLastCatMove.getxValue();
-		int catY = getLastCatMove.getyValue();
-		if ((catX == x && catY == y - 1) || (catX == x - 1 && catY == y) || (catX == x + 1 && catY == y - 1)
+	private boolean isValidCatMove(int x, int y) {
+		int catX = lastCatMove.getxValue();
+		int catY = lastCatMove.getyValue();
+		if (((catX == x && catY == y - 1) || (catX == x - 1 && catY == y) || (catX == x + 1 && catY == y - 1)
 				|| (catX == x - 1 && catY == y + 1) || (catX == x + 1 && catY == y)
-				|| (catX == x && catY == y + 1) && board[x][y].getColor().equals("Grey")) {
+				|| (catX == x && catY == y + 1)) && !board[x][y].getColor().equals("Grey")) {
 			return true;
 		}
 		return false;
+	}
+	
+	private boolean pathToEdge(int x, int y) {
+		if (board[x][y].getColor().equals("Edge")) {
+			isVisited[x][y] = true;
+			return true;
+		}else if (isVisited[x][y] == true) {
+		     return false;
+		}else if (board[x][y].getColor().equals("Grey")) {
+			isVisited[x][y] = true;
+			return false;
+		}else {
+			isVisited[x][y] = true;
+			return pathToEdge(x, y - 1) || pathToEdge(x - 1, y) || pathToEdge(x + 1, y - 1) || pathToEdge(x - 1, y + 1)
+					|| pathToEdge(x + 1, y) || pathToEdge(x, y + 1);
+		}
+	}
+
+	public String getFeedback() {
+		if (pathToEdge(lastCatMove.getxValue(), lastCatMove.getyValue()) == true) {
+			if (lastCatMove.getColor().equals("Edge"))
+				return "The Cat wins the game";
+			else if (ownerTurn == false)
+				return "It is the Cat's turn";
+			else
+				return "It is the Owner's turn";
+		} else
+			return "The Owner wins the game";
+	}
+
+	public void startNewGame() {
+		isVisited = new Boolean[11][11];
+		board = new Colors[11][11];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				Colors temp = new Colors("White", i, j);
+				board[i][j] = temp;
+				isVisited[i][j] = false;
+			}
+		}
+		board[5][5] = new Colors("Red", 5, 5);
+		lastCatMove = board[5][5];
+		setEdge();
+		setBlockedSquares();
+		ownerTurn = false;
+		pcs.firePropertyChange("New Game", null, null);
+	}
+	
+	public String getBoardColor(int i, int j) {
+		if(board[i][j].getColor() == "White") {
+			return ("-fx-background-color: white");
+		}else if(board[i][j].getColor() == "Grey") {
+			return("-fx-background-color: #A9A9A9");
+		}else if(board[i][j].getColor() == "Edge"){
+			return("-fx-background-color: #DCDCDC");
+		}else {
+			return("-fx-background-color: #FF0000");
+		}
 	}
 
 	/**
--- chatNoir/src/chatNoir/chatNoirView.java
+++ chatNoir/src/chatNoir/chatNoirView.java
@@ -6,21 +6,26 @@ import java.beans.PropertyChangeListener;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 // Red = #FF0000
 // White = #FFFFFF
 //Grey = #DCDCDC
 public class chatNoirView extends Application implements PropertyChangeListener {
 	private Model model;
-	
-	private GridPane gp;
-	
+
+	private GridPane gpGame;
+
 	private Button[][] buttonArray = new Button[11][11];
-	
-	
+
+	private Button Reset;
+
+	private Label feedBack;
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -30,20 +35,37 @@ public class chatNoirView extends Application implements PropertyChangeListener 
 		try {
 			model = new Model();
 			model.addPropertyChangeListener(this);
-			gp = new GridPane();
+			gpGame = new GridPane();
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 450, 700);
+			Scene scene = new Scene(root, 475, 750);
 			primaryStage.setTitle(model.getTitle());
-			for(int i = 0; i < buttonArray.length; i++) {
-				for(int j = 0; j < buttonArray[i].length; j++) {
+			Reset = new Button("Reset");
+			feedBack = new Label("Test");
+			feedBack.setText(model.getFeedback());
+
+			Reset.setOnAction(e -> model.startNewGame());
+			
+			//Don't want
+			for (int i = 0; i < buttonArray.length; i++) {
+				for (int j = 0; j < buttonArray[i].length; j++) {
 					buttonArray[i][j] = new Button();
-					buttonArray[i][j].setStyle(model.getBoardColor(i,j));
-					//buttonArray[i][j].setStyle("-fx-border-color: Red");
-					gp.add(buttonArray[i][j], 11 - i + j, j + i);
+					buttonArray[i][j].setStyle("-fx-border-color: Red;" + model.getBoardColor(i, j));
+					gpGame.add(buttonArray[i][j], 11 - i + j, j + i);
 				}
 			}
 			
-			root.setCenter(gp);
+			//Don't want 
+			for (int i = 0; i < buttonArray.length; i++) {
+				for (int j = 0; j < buttonArray[i].length; j++) {
+					int row = i;
+					int col = j;
+					buttonArray[row][col].setOnAction(e -> model.move(row,col));
+				}
+			}
+
+			root.setBottom(feedBack);
+			root.setTop(Reset);
+			root.setCenter(gpGame);
 			root.setStyle("-fx-background-color: #F5F5DC");
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -53,8 +75,33 @@ public class chatNoirView extends Application implements PropertyChangeListener 
 	}
 
 	@Override
-	public void propertyChange(PropertyChangeEvent arg0) {
-		// TODO Auto-generated method stub
-
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("New Game")) {
+			for (int i = 0; i < buttonArray.length; i++) {
+				for (int j = 0; j < buttonArray[i].length; j++) {
+					buttonArray[i][j] = new Button();
+					buttonArray[i][j].setStyle("-fx-border-color: Red;" + model.getBoardColor(i, j));
+					gpGame.add(buttonArray[i][j], 11 - i + j, j + i);
+					
+					//Don't want
+					int row = i;
+					int col = j;
+					buttonArray[row][col].setOnAction(e -> model.move(row,col));
+				}
+			}
+		}
+		if(evt.getPropertyName().equals("invalid")) {
+			feedBack.setText(model.getFeedback());
+		}
+		if(evt.getPropertyName().equals("valid")) {
+			for (int i = 0; i < buttonArray.length; i++) {
+				for (int j = 0; j < buttonArray[i].length; j++) {
+					buttonArray[i][j].setStyle("-fx-border-color: Red;" + model.getBoardColor(i, j));
+				}
+			}
+			feedBack.setText(model.getFeedback());
+		}
 	}
 }
+
+//model.getBoardColor(model.getLastCatMove().getxValue()
